1bca2462e61ddaa3744c32444007c997556b0412
lizeyang1995 <424011527@qq.com>
2021-01-05 17:05:41 +0800

    [lizeyang] feat: complete Base class

--- src/Base.ts
+++ src/Base.ts
@@ -1,9 +1,33 @@
 import { TYPE } from "./Tab";
 
 abstract class Base {
+    private _curIndex: number = 0
+    private _el: HTMLElement
+    private _tabItems: HTMLCollection
     constructor(el: HTMLElement, type: TYPE) {
-        console.log(el, type);
-        
+        this._el = el
+        this._tabItems = this._el.getElementsByClassName('tab-item')
+        this.init()
+    }
+
+    private init() {
+        this.bindEvent()
+    }
+
+    private bindEvent() {
+        this._el.addEventListener('click', this.setTab.bind(this), false)
+    }
+
+    private setTab(e: MouseEvent) {
+        const tar = e.target as HTMLElement
+        const className = tar.className
+
+        if (className === 'tab-item') {
+            this._tabItems[this._curIndex].className = 'tab-item'
+            //找到当前点击的那一项tar，在_tabItems里的下标
+            this._curIndex = [].indexOf.call(this._tabItems, tar)
+            this._tabItems[this._curIndex].className += ' active'
+        }
     }
 }
 
